13e20d7ef98c04b58f98d79c2082eb8437b47ee9
Armagomen <armagomen@gmail.com>
2021-06-27 14:02:25 +0300

    change message time to 60 minutes

--- mod/res/scripts/client/armagomen/battle_observer/components/donate_messages.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/donate_messages.py
@@ -84,9 +84,9 @@ class Donate(object):
         if spaceID == GuiGlobalSpaceID.LOBBY:
             currentTime = datetime.datetime.now()
             if currentTime >= self.timeDelta:
-                self.timeDelta = currentTime + datetime.timedelta(minutes=40)
+                self.timeDelta = currentTime + datetime.timedelta(minutes=60)
                 pushMessage(self.getDonateMessage(), type=SM_TYPE.Warning)
-                logInfo("A donation message has been sent to the user. Repeated in 40 minutes.")
+                logInfo("A donation message has been sent to the user. Repeated in 60 minutes.")
 
 
 donateMessage = Donate()
