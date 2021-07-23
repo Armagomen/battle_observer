444e54e0c96643df4aadfbd0140be6877d3ae79c
Armagomen <armagomen@gmail.com>
2021-07-23 15:34:26 +0300

    move and cleanup

--- mod/res/scripts/client/armagomen/battle_observer/core/observer_core.py
+++ mod/res/scripts/client/armagomen/battle_observer/core/observer_core.py
@@ -13,18 +13,15 @@ from skeletons.gui.app_loader import GuiGlobalSpaceID
 
 
 class ObserverCore(object):
-    __slots__ = ("modsDir", "gameVersion", "workingDir", "fileName", "isFileValid", "mod_version",
-                 "configLoader", "moduleLoader", "update", "componentsLoader", "limiterEnabled")
+    __slots__ = ("modsDir", "gameVersion", "workingDir", "isFileValid", "mod_version",
+                 "configLoader", "moduleLoader", "componentsLoader", "limiterEnabled")
 
     def __init__(self, configLoader):
         self.configLoader = configLoader
         self.modsDir, self.gameVersion = getCurrentModPath()
         self.workingDir = os.path.join(self.modsDir, self.gameVersion)
-        self.fileName = FILE_NAME.format(__version__)
         self.isFileValid = self.isModValidFileName()
         self.mod_version = 'v{0} - {1}'.format(__version__, self.gameVersion)
-        self.update = UpdateMain()
-        self.update.subscribe()
         self.componentsLoader = ComponentsLoader()
         self.limiterEnabled = settings.main[MAIN.ENABLE_FPS_LIMITER]
         ServicesLocator.appLoader.onGUISpaceEntered += self.onGUISpaceEntered
@@ -36,9 +33,11 @@ class ObserverCore(object):
             logInfo('MOD {0}: {1}'.format(MESSAGES.FINISH, self.mod_version))
 
     def isModValidFileName(self):
-        return self.fileName in os.listdir(self.workingDir)
+        return FILE_NAME.format(__version__) in os.listdir(self.workingDir)
 
     def start(self):
+        update = UpdateMain()
+        update.subscribe()
         if self.isFileValid:
             logInfo('MOD {0}: {1}'.format(MESSAGES.START, self.mod_version))
             self.componentsLoader.start()
@@ -50,7 +49,7 @@ class ObserverCore(object):
         if not self.isFileValid and spaceID in (GuiGlobalSpaceID.LOGIN, GuiGlobalSpaceID.LOBBY):
             from gui.Scaleform.daapi.view import dialogs
             from gui import DialogsInterface
-            locked = MESSAGES.LOCKED_BY_FILE_NAME.format(self.fileName)
+            locked = MESSAGES.LOCKED_BY_FILE_NAME.format(FILE_NAME.format(__version__))
             logWarning(locked)
             title = '{0} is locked'.format(MOD_NAME)
             btn = DialogButtons('Close')
