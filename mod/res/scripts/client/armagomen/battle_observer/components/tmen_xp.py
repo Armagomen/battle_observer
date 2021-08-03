fce6ccc81a2c5052ccc2e90acbda77d594b78fe7
Armagomen <armagomen@gmail.com>
2021-08-03 17:17:20 +0300

    update to class

--- mod/res/scripts/client/armagomen/battle_observer/components/tmen_xp.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/tmen_xp.py
@@ -2,26 +2,36 @@ from CurrentVehicle import g_currentVehicle
 from armagomen.battle_observer.settings.default_settings import settings
 from armagomen.constants import MAIN
 from armagomen.utils.common import logInfo
+from gui.Scaleform.Waiting import Waiting
 from gui.shared.gui_items.processors.vehicle import VehicleTmenXPAccelerator
 from gui.shared.utils import decorators
 from gui.veh_post_progression.models.progression import PostProgressionCompletion
 
 
-@decorators.process('updateTankmen')
-def accelerateTmenXp(vehicle, value):
-    result = yield VehicleTmenXPAccelerator(vehicle, value).request()
-    if result.success:
-        logInfo("The accelerated crew training is %s for %s" % (value, vehicle.name))
+class AccelerateTmenXp(object):
+    def __init__(self):
+        self.__vehicle = None
+        self.__value = None
+        g_currentVehicle.onChanged += self.onVehicleChanged
+
+    @decorators.process('updateTankmen')
+    def accelerateTmenXp(self, vehicle, value):
+        result = yield VehicleTmenXPAccelerator(vehicle, value).request()
+        if result.success:
+            logInfo("The accelerated crew training is %s for %s" % (value, vehicle.name))
+
+    def onVehicleChanged(self):
+        vehicle = g_currentVehicle.item
+        if not settings.main[MAIN.UNLOCK_CREW] or Waiting.isOpened('updateTankmen') or not vehicle:
+            return
+        if vehicle.postProgressionAvailability() and vehicle.isPostProgressionExists:
+            value = vehicle.postProgression.getCompletion() == PostProgressionCompletion.FULL
+            if self.__vehicle == vehicle.intCD and self.__value == value:
+                return
+            if vehicle.isXPToTman and not value or not vehicle.isXPToTman and value:
+                self.__vehicle = vehicle.intCD
+                self.__value = value
+                self.accelerateTmenXp(vehicle, value)
 
 
-def onVehicleChanged():
-    if not settings.main[MAIN.UNLOCK_CREW]:
-        return
-    vehicle = g_currentVehicle.item
-    if vehicle.postProgressionAvailability() and vehicle.isPostProgressionExists:
-        value = vehicle.postProgression.getCompletion() == PostProgressionCompletion.FULL
-        if vehicle.isXPToTman and not value or not vehicle.isXPToTman and value:
-            accelerateTmenXp(vehicle, value)
-
-
-g_currentVehicle.onChanged += onVehicleChanged
+tmenXP = AccelerateTmenXp()
