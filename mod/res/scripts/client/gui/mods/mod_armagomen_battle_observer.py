bf52a68d8f3e6a66cc6cff8c1eee7be546313042
Armagomen <armagomen@gmail.com>
2021-08-20 07:40:13 +0300

    change log level to ERROR

--- mod/res/scripts/client/gui/mods/mod_armagomen_battle_observer.py
+++ mod/res/scripts/client/gui/mods/mod_armagomen_battle_observer.py
@@ -3,4 +3,4 @@ import logging
 from armagomen.battle_observer import init, fini, onConnected, onDisconnected
 
 if callable(init) and callable(fini):
-    logging.disable(logging.WARNING)
+    logging.disable(logging.ERROR)
