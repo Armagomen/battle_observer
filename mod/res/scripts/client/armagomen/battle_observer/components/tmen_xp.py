53641d0ece14c49547c6d8051622ac2e6e9b9299
Armagomen <armagomen@gmail.com>
2021-09-11 13:02:44 +0300

    doc str

--- mod/res/scripts/client/armagomen/battle_observer/components/tmen_xp.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/tmen_xp.py
@@ -2,6 +2,7 @@ from CurrentVehicle import g_currentVehicle
 from armagomen.battle_observer.settings.default_settings import settings
 from armagomen.constants import MAIN
 from armagomen.utils.common import logInfo, callback
+from gui.shared.gui_items.Vehicle import Vehicle
 from gui.shared.gui_items.processors.vehicle import VehicleTmenXPAccelerator
 from gui.shared.utils import decorators
 from gui.veh_post_progression.models.progression import PostProgressionCompletion
@@ -19,13 +20,20 @@ class AccelerateCrewXp(object):
 
     @decorators.process('updateTankmen')
     def accelerateTmenXp(self, vehicle, value):
+        """
+        :type value: bool
+        :type vehicle: Vehicle
+        """
         result = yield VehicleTmenXPAccelerator(vehicle, value).request()
         if result.success:
-            logInfo("The accelerated crew training is %s for %s" % (value, vehicle.userName))
+            logInfo("The accelerated crew training is %s for '%s'" % (value, vehicle.userName))
         self.inProcess = False
 
     @staticmethod
     def checkXP(vehicle):
+        """
+        :type vehicle: Vehicle
+        """
         iterator = vehicle.postProgression.iterOrderedSteps()
         return vehicle.xp >= sum(x.getPrice().xp for x in iterator if not x.isRestricted() and not x.isReceived())
 
@@ -34,7 +42,7 @@ class AccelerateCrewXp(object):
             callback(1.0, self.onVehicleChanged)
 
     def onVehicleChanged(self):
-        vehicle = g_currentVehicle.item
+        vehicle = g_currentVehicle.item  # type: Vehicle
         if not vehicle.isElite or g_currentVehicle.isLocked() or g_currentVehicle.isInBattle():
             return
         value = False
