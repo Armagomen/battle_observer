dfe1f251d2e64d5f13394de2d3d343f8bafe08cf
Armagomen <armagomen@gmail.com>
2021-10-02 00:54:33 +0300

    sort lines

--- mod/res/scripts/client/armagomen/battle_observer/core/__init__.py
+++ mod/res/scripts/client/armagomen/battle_observer/core/__init__.py
@@ -4,13 +4,13 @@ try:
     from gui.modsListApi import g_modsListApi
     from gui.vxSettingsApi import vxSettingsApi, vxSettingsApiEvents
 except ImportError as err:
-    logWarning("%s: Settings API not loaded" % repr(err))
+    logWarning("%s: Settings API not found, mod not loaded, please install api packages" % repr(err))
 else:
-    from armagomen.battle_observer.settings.hangar.hangar_settings import ConfigInterface
     from armagomen.battle_observer.core.battle import BattleCore, ViewSettings
     from armagomen.battle_observer.core.observer_core import ObserverCore
     from armagomen.battle_observer.settings.config_loader import ConfigLoader
     from armagomen.battle_observer.settings.default_settings import settings
+    from armagomen.battle_observer.settings.hangar.hangar_settings import ConfigInterface
     from armagomen.utils.keys_parser import HotKeysParser
 
     c_Loader = ConfigLoader(settings)
