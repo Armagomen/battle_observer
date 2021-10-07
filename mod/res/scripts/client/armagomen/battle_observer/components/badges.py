d4dbeba792ae64c5540b5fa8268ca931752950e8
Armagomen <armagomen@gmail.com>
2021-10-07 15:16:42 +0300

    disable anti-anonymous if statistics enabled.

--- mod/res/scripts/client/armagomen/battle_observer/components/badges.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/badges.py
@@ -1,5 +1,5 @@
 from armagomen.battle_observer.core import settings, view_settings
-from armagomen.constants import MAIN, ANOTHER, GLOBAL
+from armagomen.constants import MAIN, ANOTHER, GLOBAL, STATISTICS
 from armagomen.utils.common import overrideMethod
 from gui.battle_control.arena_info import settings as arena_settings
 from gui.battle_control.arena_info.arena_vos import VehicleArenaInfoVO
@@ -7,12 +7,16 @@ from gui.battle_control.arena_info.arena_vos import VehicleArenaInfoVO
 _PLAYER_STATUS = arena_settings.PLAYER_STATUS
 
 
+def getStatisticEnabled():
+    return settings.statistics[GLOBAL.ENABLED] and settings.statistics[STATISTICS.STATISTIC_ENABLED]
+
+
 @overrideMethod(VehicleArenaInfoVO)
 def new_VehicleArenaInfoVO(init, vInfoVo, *args, **kwargs):
     if view_settings.notEpicBattle() and kwargs:
         if settings.main[MAIN.HIDE_BADGES] and ANOTHER.BADGES in kwargs:
             kwargs[ANOTHER.BADGES] = None
-        if settings.main[MAIN.SHOW_ANONYMOUS] and ANOTHER.ACCOUNT_DBID in kwargs:
+        if not getStatisticEnabled() and settings.main[MAIN.SHOW_ANONYMOUS] and ANOTHER.ACCOUNT_DBID in kwargs:
             if kwargs[ANOTHER.ACCOUNT_DBID] == GLOBAL.ZERO:
                 kwargs[ANOTHER.IS_TEAM_KILLER] = _PLAYER_STATUS.IS_TEAM_KILLER
                 if settings.main[MAIN.CHANGE_ANONYMOUS_NAME] and ANOTHER.NAME in kwargs:
