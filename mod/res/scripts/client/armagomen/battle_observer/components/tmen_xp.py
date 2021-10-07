7e10770a87343c4ce82315e04387684fd2b9b867
Armagomen <armagomen@gmail.com>
2021-10-07 16:42:43 +0300

    try fix AccelerateCrewXp synchronization

--- mod/res/scripts/client/armagomen/battle_observer/components/tmen_xp.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/tmen_xp.py
@@ -1,22 +1,19 @@
 from CurrentVehicle import g_currentVehicle
 from armagomen.battle_observer.settings.default_settings import settings
 from armagomen.constants import MAIN
-from armagomen.utils.common import logInfo, callback
+from armagomen.utils.common import logInfo, overrideMethod
+from gui.Scaleform.daapi.view.lobby.hangar.Hangar import Hangar
 from gui.shared.gui_items.Vehicle import Vehicle
 from gui.shared.gui_items.processors.vehicle import VehicleTmenXPAccelerator
 from gui.shared.utils import decorators
 from gui.veh_post_progression.models.progression import PostProgressionCompletion
-from helpers import dependency
-from skeletons.gui.shared.utils import IHangarSpace
 
 
 class AccelerateCrewXp(object):
-    hangarSpace = dependency.descriptor(IHangarSpace)
 
     def __init__(self):
         self.inProcess = False
-        self.hangarSpace.onSpaceCreating += self.onSpaceCreating
-        self.hangarSpace.onSpaceDestroy += self.onSpaceDestroy
+        overrideMethod(Hangar, "__onCurrentVehicleChanged")(self.onVehicleChanged)
 
     @decorators.process('updateTankmen')
     def accelerateTmenXp(self, vehicle, value):
@@ -37,11 +34,10 @@ class AccelerateCrewXp(object):
         iterator = vehicle.postProgression.iterOrderedSteps()
         return vehicle.xp >= sum(x.getPrice().xp for x in iterator if not x.isRestricted() and not x.isReceived())
 
-    def onChanged(self):
-        if settings.main[MAIN.CREW_TRAINING] and g_currentVehicle.isPresent() and not self.inProcess:
-            callback(1.0, self.onVehicleChanged)
-
-    def onVehicleChanged(self):
+    def onVehicleChanged(self, base, *args, **kwargs):
+        base(*args, **kwargs)
+        if not settings.main[MAIN.CREW_TRAINING] or not g_currentVehicle.isPresent() or self.inProcess:
+            return
         vehicle = g_currentVehicle.item  # type: Vehicle
         if not vehicle.isElite or g_currentVehicle.isLocked() or g_currentVehicle.isInBattle():
             return
@@ -53,12 +49,6 @@ class AccelerateCrewXp(object):
         if vehicle.isXPToTman != value:
             self.inProcess = True
             self.accelerateTmenXp(vehicle, value)
-
-    def onSpaceCreating(self):
-        g_currentVehicle.onChanged += self.onChanged
-
-    def onSpaceDestroy(self, inited):
-        g_currentVehicle.onChanged -= self.onChanged
 
 
 crewXP = AccelerateCrewXp()
