1ce3bea06a5ae717a42c37d541bd2b241cbd8882
Armagomen <armagomen@gmail.com>
2021-10-07 19:55:55 +0300

    error message load alias

--- mod/res/scripts/client/armagomen/battle_observer/core/loading_error.py
+++ mod/res/scripts/client/armagomen/battle_observer/core/loading_error.py
@@ -1,15 +1,18 @@
 # coding=utf-8
+from account_helpers.settings_core.settings_constants import GAME
 from armagomen.constants import GLOBAL
 from armagomen.utils.common import callback
 from armagomen.utils.dialogs import LoadingErrorDialog
 from frameworks.wulf import WindowLayer
 from gui.Scaleform.daapi.settings.views import VIEW_ALIAS
+from gui.shared.personality import ServicesLocator
 from helpers import dependency
 from skeletons.gui.app_loader import IAppLoader
 
 
 class LoadingError(object):
     appLoader = dependency.descriptor(IAppLoader)
+    getSetting = ServicesLocator.settingsCore.getSetting
 
     def __init__(self, errorMessage):
         if GLOBAL.RU_LOCALIZATION:
@@ -22,6 +25,7 @@ class LoadingError(object):
                            "poliroid.modslistapi*.wotmod | polarfox.vxSettingsApi*.wotmod</b></font> missing or " \
                            "damaged. Please copy correct files from the archive with the mod." \
                            "</font>\n\n{}".format(errorMessage)
+        self.loadAlias = VIEW_ALIAS.LOGIN if self.getSetting(GAME.LOGIN_SERVER_SELECTION) else VIEW_ALIAS.LOBBY_HANGAR
         self.waitingLoginLoaded()
 
     def waitingLoginLoaded(self):
@@ -30,7 +34,7 @@ class LoadingError(object):
             callback(2.0, self.waitingLoginLoaded)
             return
         view = app.containerManager.getView(WindowLayer.VIEW)
-        if view and view.settings.alias == VIEW_ALIAS.LOGIN and view.isCreated():
+        if view and view.settings.alias == self.loadAlias and view.isCreated():
             dialog = LoadingErrorDialog()
             dialog.setView(view)
             dialog.showLoadingError(self.message)
