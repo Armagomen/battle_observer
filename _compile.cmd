ae012ede9580ee61eb409fbc5549fc5e969e335b
Armagomen <armagomen@gmail.com>
2021-11-03 23:33:38 +0200

    1.35.8 - 1.14.1.3

--- _compile.cmd
+++ _compile.cmd
@@ -1,7 +1,7 @@
 @Echo off
-set ModVer=1.35.7
+set ModVer=1.35.8
 
-set WotVer=1.14.1.2
+set WotVer=1.14.1.3
 set ModsDir=C:\Games\World_of_Tanks_RU\mods
 
 REM set WotVer="1.14.0.0 Common Test"
--- mod/res/scripts/client/armagomen/battle_observer/__init__.py
+++ mod/res/scripts/client/armagomen/battle_observer/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Armagomen"
-__version__ = "1.35.7"
+__version__ = "1.35.8"
 __copyright__ = "Copyright 2014-2021, Armagomen"
 __credits__ = ["Armagomen"]
 __license__ = "CC BY-NC-SA 4.0"
