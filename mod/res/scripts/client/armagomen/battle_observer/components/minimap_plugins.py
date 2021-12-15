b84851845c1aeefae4b2c4510b5768b9211d6c86
Armagomen <armagomen@gmail.com>
2021-12-15 14:11:51 +0200

    minimap death

--- mod/res/scripts/client/armagomen/battle_observer/components/minimap_plugins.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/minimap_plugins.py
@@ -36,40 +36,30 @@ class ArenaVehiclesPlugin(plugins.ArenaVehiclesPlugin):
         self.__isDestroyImmediately = settings.minimap[MINIMAP.DEATH_PERMANENT]
 
     def _showVehicle(self, vehicleID, location):
-        entry = self._entries.get(vehicleID, None)
-        if entry is None or not entry.isAlive():
-            return
-        matrix = matrix_factory.makeVehicleMPByLocation(vehicleID, location, self._arenaVisitor.getArenaPositions())
-        if matrix is not None:
-            self.__setLocationAndMatrix(entry, location, matrix)
-            self._setInAoI(entry, True)
-            self.__setActive(entry, True)
-            self.__showVehicleHp(vehicleID, entry.getID())
+        entry = self._entries[vehicleID]
+        if entry.isAlive():
+            super(ArenaVehiclesPlugin, self)._showVehicle(vehicleID, location)
 
     def _hideVehicle(self, entry):
         if entry.isAlive() and entry.isActive():
-            matrix = entry.getMatrix()
-            if matrix is not None:
-                matrix = matrix_factory.convertToLastSpottedVehicleMP(matrix)
-            self._setInAoI(entry, False)
-            self.__setLocationAndMatrix(entry, VEHICLE_LOCATION.UNDEFINED, matrix)
+            super(ArenaVehiclesPlugin, self)._hideVehicle(entry)
 
     def __setDestroyed(self, vehicleID, entry):
         self.__clearAoIToFarCallback(vehicleID)
-        if not entry.wasSpotted() and entry.setAlive(False) and entry.getMatrix() is not None:
+        if entry.setAlive(False) and not entry.wasSpotted():
             if not entry.isActive():
                 self.__setActive(entry, True)
             if entry.isActive() and not entry.isInAoI():
                 self._setInAoI(entry, True)
-            self._invoke(entry._entryID, 'setDead', True)
-            self._move(entry._entryID, CONTAINER_NAME.DEAD_VEHICLES)
-            self._invoke(entry._entryID, self._showNames)
+            self._invoke(entry.getID(), 'setDead', True)
+            self._move(entry.getID(), CONTAINER_NAME.DEAD_VEHICLES)
+            self._invoke(entry.getID(), self._showNames)
         else:
             self.__setActive(entry, False)
 
     @property
     def _showNames(self):
-        if settings.minimap[MINIMAP.DEATH_PERMANENT] and settings.minimap[MINIMAP.SHOW_NAMES]:
+        if self.__showDestroyEntries and settings.minimap[MINIMAP.SHOW_NAMES]:
             return 'showVehicleName'
         return 'hideVehicleName'
 
