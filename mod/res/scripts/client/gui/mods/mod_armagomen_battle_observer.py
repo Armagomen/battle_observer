d5333e7942a6778cc3844b5b193729cfbe9cedef
Armagomen <armagomen@gmail.com>
2022-03-24 15:48:17 +0200

    check locale 2

--- mod/res/scripts/client/gui/mods/mod_armagomen_battle_observer.py
+++ mod/res/scripts/client/gui/mods/mod_armagomen_battle_observer.py
@@ -1,6 +1,13 @@
 import locale
 
-if locale.getdefaultlocale()[0] not in ("ba_RU", "ru_RU", "tt_RU", "sah_RU"):
+locations = (
+    locale.windows_locale[0x046d],  # Bashkir ba_RU
+    locale.windows_locale[0x0419],  # Russian ru_RU
+    locale.windows_locale[0x0444],  # Tatar tt_RU
+    locale.windows_locale[0x0485]   # Yakut - Cyrillic sah_RU
+)
+
+if locale.getdefaultlocale()[0] not in locations:
     import logging
 
     from armagomen.battle_observer import init, fini
