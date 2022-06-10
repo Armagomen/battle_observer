7206e651ad9e02616529a8b957121f2fd80d9495
Armagomen <armagomen@gmail.com>
2022-06-10 17:32:27 +0300

    delete region check

--- mod/res/scripts/client/gui/mods/mod_armagomen_battle_observer.py
+++ mod/res/scripts/client/gui/mods/mod_armagomen_battle_observer.py
@@ -1,19 +1,9 @@
-import locale
+import logging
 
-locations = (
-    locale.windows_locale[0x046d],  # Bashkir ba_RU
-    locale.windows_locale[0x0419],  # Russian ru_RU
-    locale.windows_locale[0x0444],  # Tatar tt_RU
-    locale.windows_locale[0x0485]   # Yakut - Cyrillic sah_RU
-)
+from armagomen.battle_observer import init, fini
+from helpers.statistics import StatisticsCollector
 
-if locale.getdefaultlocale()[0] not in locations:
-    import logging
+if callable(init) and callable(fini):
+    logging.disable(logging.ERROR)
 
-    from armagomen.battle_observer import init, fini
-    from helpers.statistics import StatisticsCollector
-
-    if callable(init) and callable(fini):
-        logging.disable(logging.ERROR)
-
-    StatisticsCollector.noteHangarLoadingState = lambda *args, **kwargs: None
+StatisticsCollector.noteHangarLoadingState = lambda *args, **kwargs: None
