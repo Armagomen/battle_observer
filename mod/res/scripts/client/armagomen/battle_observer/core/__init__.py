1a8b4c282bd6e98c2ce9b043526c7c73f0c035f0
Armagomen <armagomen@gmail.com>
2022-09-22 16:14:56 +0300

    import api

--- mod/res/scripts/client/armagomen/battle_observer/core/__init__.py
+++ mod/res/scripts/client/armagomen/battle_observer/core/__init__.py
@@ -7,24 +7,36 @@ errorMessage = ""
 
 try:
     from armagomen.battle_observer.core.update.worker import UpdateMain
+
     update = UpdateMain(__version__)
-except:
+except Exception:
     LOG_CURRENT_EXCEPTION()
 
+
+def importApi():
+    try:
+        from gui.modsListApi import g_modsListApi
+        from gui.vxSettingsApi import vxSettingsApi, vxSettingsApiEvents
+    except Exception:
+        LOG_CURRENT_EXCEPTION()
+        return None, None, None
+    else:
+        return g_modsListApi, vxSettingsApi, vxSettingsApiEvents
+
+
+g_modsListApi, vxSettingsApi, vxSettingsApiEvents = importApi()
+
 try:
-    from armagomen.utils.common import isFileValid, clearClientCache, cleanupUpdates, logInfo, logError, gameVersion
-    from gui.modsListApi import g_modsListApi
-    from gui.vxSettingsApi import vxSettingsApi, vxSettingsApiEvents
-    from sys import version as python_version
-    from armagomen.battle_observer.core.view_settings import ViewSettings
     from armagomen.battle_observer.components import ComponentsLoader
-    from armagomen.battle_observer.settings.loader import SettingsLoader
+    from armagomen.battle_observer.core.view_settings import ViewSettings
     from armagomen.battle_observer.settings.hangar.hangar_settings import SettingsInterface
+    from armagomen.battle_observer.settings.loader import SettingsLoader
+    from armagomen.utils.common import isFileValid, clearClientCache, cleanupUpdates, logInfo, logError, gameVersion
+    from sys import version as python_version
 except Exception as err:
     LOG_CURRENT_EXCEPTION()
     loadError = True
     errorMessage = repr(err)
-    logError(errorMessage)
 else:
     if isFileValid(__version__):
         logInfo("Launched at python v{}".format(python_version))
@@ -32,8 +44,9 @@ else:
         _view_settings = ViewSettings()
         componentsLoader = ComponentsLoader()
         settings_loader = SettingsLoader()
-        hangar_settings = SettingsInterface(g_modsListApi, vxSettingsApi, vxSettingsApiEvents,
-                                            settings_loader, __version__)
+        if g_modsListApi is not None and vxSettingsApi is not None and vxSettingsApiEvents is not None:
+            hangar_settings = SettingsInterface(g_modsListApi, vxSettingsApi, vxSettingsApiEvents,
+                                                settings_loader, __version__)
     else:
         loadError = True
         errorMessage = "ERROR: file armagomen.battleObserver_{}.wotmod is not valid, mod locked, please " \
