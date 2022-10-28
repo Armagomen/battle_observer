be255fc78f0413d25a19d8af27f4682ecf924756
Armagomen <armagomen@gmail.com>
2022-10-28 20:41:44 +0300

    anonymous

--- mod/res/scripts/client/armagomen/battle_observer/components/replace_vehicle_info.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/replace_vehicle_info.py
@@ -16,7 +16,7 @@ def new_VehicleArenaInfoVO(init, vInfoVo, *args, **kwargs):
         if not viewSettings.isWTREnabled() and settings.main[MAIN.SHOW_ANONYMOUS] and ANOTHER.ACCOUNT_DBID in kwargs:
             if kwargs[ANOTHER.ACCOUNT_DBID] == GLOBAL.ZERO:
                 kwargs[ANOTHER.IS_TEAM_KILLER] = PLAYER_STATUS.IS_TEAM_KILLER
-            if ANOTHER.NAME in kwargs and not kwargs[ANOTHER.NAME].startswith(BOT_START_SWITCH):
+            if ANOTHER.NAME in kwargs and BOT_START_SWITCH not in kwargs[ANOTHER.NAME]:
                 kwargs[ANOTHER.NAME] = kwargs[ANOTHER.FAKE_NAME] = "Anonymous"
         if settings.main[MAIN.HIDE_CLAN_ABBREV] and ANOTHER.CLAN_DBID in kwargs and ANOTHER.CLAN_ABBR in kwargs:
             kwargs[ANOTHER.CLAN_DBID] = GLOBAL.FIRST
