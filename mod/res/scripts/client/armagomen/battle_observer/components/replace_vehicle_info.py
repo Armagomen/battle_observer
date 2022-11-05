3b1fb9621abf11735405925beac204a9ccd50d1d
Armagomen <armagomen@gmail.com>
2022-11-05 11:44:40 +0200

    Anonymous localization

--- mod/res/scripts/client/armagomen/battle_observer/components/replace_vehicle_info.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/replace_vehicle_info.py
@@ -1,11 +1,25 @@
+# coding=utf-8
+from collections import defaultdict
+
 from armagomen.battle_observer.core import viewSettings
 from armagomen.battle_observer.settings.default_settings import settings
 from armagomen.constants import MAIN, ANOTHER, GLOBAL
 from armagomen.utils.common import overrideMethod
 from gui.battle_control.arena_info.arena_vos import VehicleArenaInfoVO
 from gui.battle_control.arena_info.settings import PLAYER_STATUS
+from helpers import getClientLanguage
 
+language = getClientLanguage()
 BOT_START_SWITCH = ":"
+
+localization = {"uk": "Анонімний", "ru": "Анонимный", "be": "Ананімны"}
+ANONYMOUS_TRANSLATE = defaultdict(lambda: "Anonymous", **localization)
+
+
+def getName(name):
+    if BOT_START_SWITCH in name:
+        return "BOT"
+    return ANONYMOUS_TRANSLATE[language]
 
 
 @overrideMethod(VehicleArenaInfoVO)
@@ -16,7 +30,7 @@ def new_VehicleArenaInfoVO(init, vInfoVo, *args, **kwargs):
         if settings.main[MAIN.SHOW_ANONYMOUS] and not viewSettings.isWTREnabled() and ANOTHER.ACCOUNT_DBID in kwargs:
             if kwargs[ANOTHER.ACCOUNT_DBID] == GLOBAL.ZERO:
                 kwargs[ANOTHER.IS_TEAM_KILLER] = PLAYER_STATUS.IS_TEAM_KILLER
-                kwargs[ANOTHER.NAME] = kwargs[ANOTHER.FAKE_NAME] = "Anonymous"
+                kwargs[ANOTHER.NAME] = kwargs[ANOTHER.FAKE_NAME] = getName(kwargs[ANOTHER.NAME])
         if settings.main[MAIN.HIDE_CLAN_ABBREV] and ANOTHER.CLAN_DBID in kwargs and ANOTHER.CLAN_ABBR in kwargs:
             kwargs[ANOTHER.CLAN_DBID] = GLOBAL.FIRST
             kwargs[ANOTHER.CLAN_ABBR] = GLOBAL.EMPTY_LINE
