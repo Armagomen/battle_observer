2b97dcf8668b746ac5b8d70f423edb2f1061b298
Armagomen <armagomen@gmail.com>
2022-11-14 16:34:04 +0200

    vehicle name

--- mod/res/scripts/client/armagomen/battle_observer/core/current_vehicle_data.py
+++ mod/res/scripts/client/armagomen/battle_observer/core/current_vehicle_data.py
@@ -8,14 +8,14 @@ from helpers import dependency
 from skeletons.gui.shared import IItemsCache
 
 EfficiencyAVGData = namedtuple("EfficiencyAVGData", (
-    "damage", "assist", "stun", "blocked", "marksOnGunValue", "marksOnGunIcon"))
+    "damage", "assist", "stun", "blocked", "marksOnGunValue", "marksOnGunIcon", "name"))
 
 
 class CurrentVehicleCachedData(object):
     itemsCache = dependency.descriptor(IItemsCache)
 
     def __init__(self):
-        self.__EfficiencyAVGData = EfficiencyAVGData(0, 0, 0, 0, 0, "")
+        self.__EfficiencyAVGData = EfficiencyAVGData(0, 0, 0, 0, 0, "", "Undefined")
 
     def init(self):
         g_events.onVehicleChanged += self.onVehicleChanged
@@ -25,20 +25,20 @@ class CurrentVehicleCachedData(object):
 
     def onVehicleChanged(self):
         if g_currentVehicle.isPresent():
-            self.setAvgData(g_currentVehicle.intCD)
+            self.setAvgData(g_currentVehicle.intCD, g_currentVehicle.item.userName)
 
-    def setAvgData(self, intCD):
+    def setAvgData(self, intCD, name):
         dossier = self.itemsCache.items.getVehicleDossier(intCD)
         random = dossier.getRandomStats()
         marksOnGun = random.getAchievement(MARK_ON_GUN_RECORD)
         icon = marksOnGun.getIcons()['95x85'][3:]
-        marksOnGunIcon = "<img src='img://gui/{}' width='20' height='20' vspace='-10'>".format(icon)
+        marksOnGunIcon = "<img src='img://gui/{}' width='22' height='20' vspace='-10'>".format(icon)
         self.__EfficiencyAVGData = EfficiencyAVGData(
             int(random.getAvgDamage() or 0),
             int(random.getDamageAssistedEfficiency() or 0),
             int(random.getAvgDamageAssistedStun() or 0),
             int(random.getAvgDamageBlocked() or 0),
-            round(marksOnGun.getDamageRating(), 2), marksOnGunIcon)
+            round(marksOnGun.getDamageRating(), 2), marksOnGunIcon, name)
         logDebug(self.__EfficiencyAVGData)
         g_events.onAVGDataUpdated(self.__EfficiencyAVGData)
 
