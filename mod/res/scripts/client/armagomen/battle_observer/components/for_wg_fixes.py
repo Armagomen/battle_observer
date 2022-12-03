976347dc138ed26efed7125845d0b1558d0f872f
Armagomen <armagomen@gmail.com>
2022-12-03 12:04:19 +0200

    remove front line fixes

--- mod/res/scripts/client/armagomen/battle_observer/components/for_wg_fixes.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/for_wg_fixes.py
@@ -3,12 +3,7 @@ import BigWorld
 from BattleReplay import g_replayCtrl, isServerSideReplay
 from DogTagComponent import DogTagComponent
 from armagomen.utils.common import overrideMethod
-from avatar_components.avatar_chat_key_handling import AvatarChatKeyHandling, _logger
-from chat_commands_consts import _PERSONAL_MESSAGE_MUTE_DURATION
-from gui.Scaleform.daapi.view.battle.shared.markers2d.vehicle_plugins import VehicleMarkerPlugin, MarkerState
-from gui.Scaleform.genConsts.BATTLE_MARKER_STATES import BATTLE_MARKER_STATES
 from gui.battle_control.controllers import debug_ctrl
-from gui.sounds.epic_sound_constants import EPIC_SOUND
 
 debug_ctrl._UPDATE_INTERVAL = 0.5
 
@@ -53,64 +48,3 @@ def _isObserving(base, *args):
         if vehicle is not None:
             return not vehicle.isPlayerVehicle
     return False
-
-
-# fix AttributeError: 'NoneType' object has no attribute 'translation' in AvatarChatKeyHandling
-@overrideMethod(AvatarChatKeyHandling, "__playSoundNotificationOnCommandReceived")
-def playSoundNotificationOnCommandReceived(base, self, cmd, markerType, useSoundNotification=False,
-                                           notificationName=None, enableVoice=True):
-    if cmd.isEpicGlobalMessage():
-        if self.soundNotifications:
-            self.soundNotifications.play(EPIC_SOUND.BF_EB_GLOBAL_MESSAGE)
-    else:
-        matrixProvider = self._AvatarChatKeyHandling__getMatrixProvider(cmd, markerType).matrixProvider
-        if matrixProvider is None:
-            return
-        if notificationName is None:
-            notificationName = cmd.getSoundEventName(useSoundNotification)
-        if enableVoice and cmd.isReceiver():
-            if not self._AvatarChatKeyHandling__arePrivateVoiceOverBlocked:
-                self._AvatarChatKeyHandling__arePrivateVoiceOverBlocked = True
-                self._AvatarChatKeyHandling__callbackDelayer.delayCallback(
-                    _PERSONAL_MESSAGE_MUTE_DURATION, self._AvatarChatKeyHandling__onPrivateVoiceOverBlockedReset)
-            else:
-                enableVoice = False
-                _logger.info('Voice was blocked for the receiver of a private message due to flood prevention system!')
-        sentByPlayer = self._AvatarChatKeyHandling__getVehicleIDForCmdSender(cmd) == self.playerVehicleID
-        self._AvatarChatKeyHandling__playSoundNotification(notificationName, matrixProvider.translation, enableVoice,
-                                                           sentByPlayer)
-
-
-# ValueError: list.remove(x): x not in list in VehicleMarkerPlugin
-@overrideMethod(VehicleMarkerPlugin, "_updateStatusMarkerState")
-def _updateStatusMarkerState(base, self, vehicleID, isShown, handle, statusID, duration, animated, isSourceVehicle,
-                             blinkAnim=True):
-    activeStatuses = self._markersStates[vehicleID]
-    marker = MarkerState(statusID, isSourceVehicle)
-    isStatusActive = self._VehicleMarkerPlugin__isStatusActive(statusID, activeStatuses)
-    if isShown and not isStatusActive:
-        activeStatuses.append(marker)
-        self._markersStates[vehicleID] = activeStatuses
-    elif not isShown and isStatusActive and marker in self._markersStates[vehicleID]:
-        self._markersStates[vehicleID].remove(marker)
-    if self._markersStates[vehicleID]:
-        activeStatuses = sorted(self._markersStates[vehicleID], key=self._getMarkerStatusPriority, reverse=False)
-        self._markersStates[vehicleID] = activeStatuses
-        currentlyActiveStatusID = self._markersStates[vehicleID][0].statusID
-        currentlyActiveIsSourceVehicle = self._markersStates[vehicleID][0].isSourceVehicle
-    else:
-        currentlyActiveStatusID = -1
-        currentlyActiveIsSourceVehicle = False
-    currentActiveMarker = MarkerState(currentlyActiveStatusID, currentlyActiveIsSourceVehicle)
-    if statusID in (BATTLE_MARKER_STATES.STUN_STATE, BATTLE_MARKER_STATES.HEALING_STATE):
-        isSourceVehicle = True
-    elif statusID == BATTLE_MARKER_STATES.DEBUFF_STATE:
-        isSourceVehicle = False
-    if isShown:
-        self._invokeMarker(handle, 'showStatusMarker', statusID,
-                           self._getMarkerStatusPriority(MarkerState(statusID, isSourceVehicle)), isSourceVehicle,
-                           duration, currentlyActiveStatusID, self._getMarkerStatusPriority(currentActiveMarker),
-                           animated, blinkAnim)
-    elif self._VehicleMarkerPlugin__canUpdateStatus(handle):
-        self._invokeMarker(handle, 'hideStatusMarker', statusID, currentlyActiveStatusID, animated,
-                           currentlyActiveIsSourceVehicle)
