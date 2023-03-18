422c2519347d5b9648245bf90ec53c36610daf2f
Armagomen <armagomen@gmail.com>
2023-03-18 12:56:06 +0200

    replay check

--- mod/res/scripts/client/armagomen/battle_observer/components/friends.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/friends.py
@@ -2,7 +2,7 @@ from PlayerEvents import g_playerEvents
 from armagomen.battle_observer.core import viewSettings
 from armagomen.battle_observer.settings.default_settings import settings
 from armagomen.constants import MAIN, ANOTHER, GLOBAL
-from armagomen.utils.common import overrideMethod
+from armagomen.utils.common import overrideMethod, isReplay
 from gui.battle_control.arena_info.arena_vos import VehicleTypeInfoVO
 from messenger.gui.Scaleform.data.contacts_data_provider import _ContactsCategories
 from messenger.storage import storage_getter
@@ -22,7 +22,7 @@ g_playerEvents.onGuiCacheSyncCompleted += onGuiCacheSyncCompleted
 
 @overrideMethod(VehicleTypeInfoVO)
 def new_VehicleArenaInfoVO(init, vTypeVo, *args, **kwargs):
-    if not viewSettings.gui.isInEpicRange() and settings.main[MAIN.SHOW_FRIENDS]:
+    if not isReplay() and not viewSettings.gui.isInEpicRange() and settings.main[MAIN.SHOW_FRIENDS]:
         init(vTypeVo, *args, **kwargs)
         if kwargs.get(ANOTHER.ACCOUNT_DBID) and not vTypeVo.isPremiumIGR:
             vTypeVo.isPremiumIGR = kwargs[ANOTHER.ACCOUNT_DBID] in _cache
@@ -32,7 +32,7 @@ def new_VehicleArenaInfoVO(init, vTypeVo, *args, **kwargs):
 
 @overrideMethod(VehicleTypeInfoVO, "update")
 def new_VehicleTypeInfoVO_update(update, vTypeVo, *args, **kwargs):
-    if not viewSettings.gui.isInEpicRange() and settings.main[MAIN.SHOW_FRIENDS]:
+    if not isReplay() and not viewSettings.gui.isInEpicRange() and settings.main[MAIN.SHOW_FRIENDS]:
         isPremiumIGR = getattr(vTypeVo, "isPremiumIGR", False)
         result = update(vTypeVo, *args, **kwargs)
         if kwargs.get('vehicleType') is not None and hasattr(vTypeVo, "isPremiumIGR") and isPremiumIGR:
