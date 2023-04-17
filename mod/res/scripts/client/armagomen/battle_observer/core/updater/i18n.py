0e5a7b91860f25e8b1dd05cb765a0261d4dea178
Armagomen <armagomen@gmail.com>
2023-04-18 00:07:18 +0300

    updater i18n

--- mod/res/scripts/client/armagomen/battle_observer/core/updater/i18n.py
+++ mod/res/scripts/client/armagomen/battle_observer/core/updater/i18n.py
@@ -8,8 +8,8 @@ en = {
         "<font size='15'>"
         "<font size='20' color='#FFFF00'><b>Choose a download option.</b></font>\n"
         "<p align='left'><font color ='#00FF00'><b>Automatically</b></font> - download and unpack the archive"
-        "updates to the folder {0}\n<font color='#FFFF00'><b>Manually</b></font> - will open a link to"
-        "the same archive, but you will have to extract the package and copy the *.wotmod file manually.</p>"
+        "updates to the folder {0}\n<font color='#FFFF00'><b>Manually</b></font> - opens a link to the installer in "
+        "the browser, you will need to install manually.</p>"
         "\n<font size='20 'color='#FFFF00'><b>Changelog</b></font>\n"
         "<p align='left'>{1}</p>"
         "</font>",
@@ -23,8 +23,8 @@ uk = {
         "<font size='15'>"
         "<font size='20' color='#FFFF00'><b>Оберіть варіант завантаження.</b></font>\n"
         "<p align='left'><font color='#00FF00'><b>Автоматично</b></font> - завантажить і розпакує архів "
-        "оновлення в папку. {0}\n<font color='#FFFF00'><b>Вручну</b></font> - відкриє в браузері посилання на "
-        "цей же архів, але витягувати пакет і копіювати *.wotmod файл потрібно буде в ручному режимі.</p>"
+        "оновлення в папку. {0}\n<font color='#FFFF00'><b>Вручну</b></font> - відкриває в браузері посилання на "
+        "інсталятор, встановлювати потрібно буде в ручному режимі.</p>"
         "\n<font size='20' color='#FFFF00'><b>Список змін</b></font>\n"
         "<p align='left'>{1}</p>"
         "</font>",
