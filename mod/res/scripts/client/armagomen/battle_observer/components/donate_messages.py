609a23f61eb70a95d265d1b4a0e432f4d2f5bf7d
Armagomen <armagomen@gmail.com>
2024-06-18 16:43:12 +0300

    show clan invite only if len members < 99

--- mod/res/scripts/client/armagomen/battle_observer/components/donate_messages.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/donate_messages.py
@@ -1,9 +1,10 @@
 # coding=utf-8
+import json
 from datetime import datetime, timedelta
 from random import choice
 
 from armagomen._constants import getLogo, IMG, URLS
-from armagomen.utils.common import logInfo, openWebBrowser, overrideMethod
+from armagomen.utils.common import fetchURL, logDebug, logInfo, openWebBrowser, overrideMethod
 from constants import AUTH_REALM
 from gui.clans.clan_cache import g_clanCache
 from gui.shared import event_dispatcher
@@ -13,6 +14,7 @@ from helpers import getClientLanguage
 from notification.NotificationListView import NotificationListView
 from notification.NotificationPopUpViewer import NotificationPopUpViewer
 from skeletons.gui.app_loader import GuiGlobalSpaceID
+from uilogging.core.core_constants import HTTP_OK_STATUS
 
 CLAN_ABBREV = "BO-UA"
 
@@ -38,16 +40,29 @@ LINKS_FORMAT = {
     "en": {"url": URLS.PATREON, "img": IMG.PATREON, "name": "PATREON"},
 }
 
+API_URL = ("https://api.worldoftanks.eu/wot/clans/info/?"
+           "application_id=5500d1b937426e47e2b039e4a11990be&clan_id=500223690&fields=members_count")
+
 
 class Donate(object):
 
-    def __init__(self, ln):
-        self.messages = MESSAGES[ln]
-        self.message_format = LINKS_FORMAT[ln]
+    def __init__(self):
+        self.ln_code = "uk" if getClientLanguage().lower() in ("uk", "be", "ru") else "en"
+        self.messages = MESSAGES[self.ln_code]
+        self.message_format = LINKS_FORMAT[self.ln_code]
         self.timeDelta = datetime.now() + timedelta(minutes=5)
         self.lastMessage = None
+        self.show_clan_invite = True
         ServicesLocator.appLoader.onGUISpaceEntered += self.pushNewMessage
-        self.show_clanMessage = ln == "uk"
+        fetchURL(API_URL, self.onDataResponse)
+
+    def onDataResponse(self, response):
+        if response.responseCode == HTTP_OK_STATUS:
+            response_data = json.loads(response.body)
+            data = response_data.get("data", {})
+            logDebug("Donate/onDataResponse: FINISH request clan data={}", data)
+            if data:
+                self.show_clan_invite = data["500223690"]["members_count"] < 99
 
     def getRandomMessage(self):
         message = choice(self.messages)
@@ -69,20 +84,20 @@ class Donate(object):
         pushMessage(message, type=SM_TYPE.Warning)
 
     def pushNewMessage(self, spaceID):
-        show = g_clanCache.clanAbbrev is None or "WG" not in g_clanCache.clanAbbrev
+        show = not g_clanCache.isInClan or "WG" not in g_clanCache.clanAbbrev
         if spaceID == GuiGlobalSpaceID.LOBBY and show:
             current_time = datetime.now()
             if current_time >= self.timeDelta:
                 self.timeDelta = current_time + timedelta(hours=1)
                 pushMessage(self.getDonateMessage(), type=SM_TYPE.Warning)
                 logInfo("A donation message has been sent to the user. Repeated in 1 hour.")
-                if self.show_clanMessage and g_clanCache.clanAbbrev is None:
-                    self.pushClanMessage()
+            if self.show_clan_invite and not g_clanCache.isInClan and self.ln_code == "uk":
+                self.pushClanMessage()
+                self.show_clan_invite = False
 
 
 if AUTH_REALM == "EU":
-    ln_code = "uk" if getClientLanguage().lower() in ("uk", "be", "ru") else "en"
-    dn = Donate(ln_code)
+    dn = Donate()
 
 
     @overrideMethod(NotificationListView, "onClickAction")
