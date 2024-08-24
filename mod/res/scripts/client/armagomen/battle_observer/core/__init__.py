03048078f1792f3ca2271feb5f0ae70baa74c607
Armagomen <armagomen@gmail.com>
2024-08-24 23:35:28 +0300

    1.41.26

--- mod/res/scripts/client/armagomen/battle_observer/core/__init__.py
+++ mod/res/scripts/client/armagomen/battle_observer/core/__init__.py
@@ -19,8 +19,8 @@ class Core(object):
         from armagomen._constants import BATTLES_RANGE
         from gui.override_scaleform_views_manager import g_overrideScaleFormViewsConfig
         from gui.Scaleform.required_libraries_config import BATTLE_REQUIRED_LIBRARIES, LOBBY_REQUIRED_LIBRARIES
-        BATTLE_REQUIRED_LIBRARIES.append('modBattleObserver.swf')
-        LOBBY_REQUIRED_LIBRARIES.append('modBattleObserverHangar.swf')
+        BATTLE_REQUIRED_LIBRARIES.insert(0, 'modBattleObserver.swf')
+        LOBBY_REQUIRED_LIBRARIES.insert(0, 'modBattleObserverHangar.swf')
         for guiType in BATTLES_RANGE:
             g_overrideScaleFormViewsConfig.battlePackages[guiType].append("armagomen.battle_observer.battle")
         g_overrideScaleFormViewsConfig.lobbyPackages.append("armagomen.battle_observer.lobby")
