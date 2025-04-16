f12af07350e9bed4aa5c344424f8f64b2eeb0b61
Armagomen <armagomen@gmail.com>
2025-04-16 22:58:25 +0300

    autoupdate only if wg client

--- mod/res/scripts/client/gui/mods/mod_armagomen_battle_observer.py
+++ mod/res/scripts/client/gui/mods/mod_armagomen_battle_observer.py
@@ -20,14 +20,17 @@ __mod = []
 
 
 def init():
+    from armagomen._constants import IS_WG_CLIENT
     from armagomen.battle_observer.core import Core
-    from armagomen.battle_observer.core.updater import Updater
     logInfo('MOD START LOADING: v{}', __version__)
     logInfo('Launched at python v{} region={}', version, CURRENT_REALM)
-    __mod.extend((Core(), Updater()))
+    __mod.append(Core())
+    if IS_WG_CLIENT:
+        from armagomen.battle_observer.core.updater import Updater
+        __mod.append(Updater())
+
     for component in __mod:
         component.start(__version__)
-
 
 def fini():
     for component in __mod:
