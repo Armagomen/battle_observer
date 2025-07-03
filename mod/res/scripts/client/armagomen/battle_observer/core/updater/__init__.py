d96cbb7a7136622d4de33afce456fd67b02870ce
Armagomen <armagomen@gmail.com>
2025-07-03 12:01:49 +0300

    updater

--- mod/res/scripts/client/armagomen/battle_observer/core/updater/__init__.py
+++ mod/res/scripts/client/armagomen/battle_observer/core/updater/__init__.py
@@ -1,9 +1,6 @@
-# coding=utf-8
 import os
 import re
-import shutil
 import subprocess
-import urllib
 from collections import namedtuple
 from datetime import datetime, timedelta
 from json import loads
@@ -36,25 +33,12 @@ __MESSAGES = (
     'DownloadThread: added new file {}',
     'DownloadThread: update is already downloaded to: {}',
     'DownloadThread: downloading update finished to: {}',
-    'DownloadThread: update download failed: {}'
+    'DownloadThread: downloading failed: {}'
 )
 
 LOG_MESSAGES = namedtuple("MESSAGES", __NAMES)(*__MESSAGES)
 EXE_FILE = "{0}/mod_battle_observer_v{0}.exe"
 ZIP = "{0}/AutoUpdate.zip"
-
-
-def ensure_cleanup_exe(local_path, remote_url):
-    if os.path.exists(local_path):
-        return True
-    try:
-        response = urllib.urlopen(remote_url)
-        with open(local_path, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
-        return True
-    except Exception as e:
-        logError("ensure_cleanup_exe {}", str(e))
-        return False
 
 
 class DownloadThread(object):
@@ -64,33 +48,62 @@ class DownloadThread(object):
         self.i18n = getI18n()
         self.updateData = dict()
         self.dialogs = UpdaterDialogs()
-        self.downloader = None
         self.modPath = os.path.join(MODS_PATH, GAME_VERSION)
         self.isReplay = isReplay()
         self.cleanup_exe = os.path.join(getObserverCachePath(), "cleanup_launcher.exe")
-        exe_url = "https://raw.githubusercontent.com/Armagomen/auto-cleanup-observer/master/dist/cleanup_launcher.exe"
-        self.hasCleanupFile = ensure_cleanup_exe(self.cleanup_exe, exe_url)
+        self.cleanup_launcher_exist = os.path.isfile(self.cleanup_exe)
+        if not self.cleanup_launcher_exist:
+            self.download_cleanup_exe(self.cleanup_exe)
 
-    def startDownload(self, version):
+    def download_cleanup_exe(self, local_path):
+        downloader = WebDownloader(1)
+        exe_url = "https://raw.githubusercontent.com/Armagomen/auto-cleanup-observer/master/dist/cleanup_launcher.exe"
+
+        def onDownload(_url, data):
+            if data is not None:
+                with open(local_path, 'wb') as out_file:
+                    out_file.write(data)
+                self.cleanup_launcher_exist = True
+            else:
+                self.downloadError(_url)
+            downloader.close()
+
+        downloader.download(exe_url, onDownload)
+
+    def startDownloadingUpdate(self, version):
         path = os.path.join(getUpdatePath(), version + ".zip")
         if os.path.isfile(path):
-            self.extractZipArchive(path)
-            logInfo(LOG_MESSAGES.ALREADY_DOWNLOADED, path)
-            git_message = re.sub(r'^\s+|\r|\t|\s+$', GLOBAL.EMPTY_LINE, self.updateData.get('body', GLOBAL.EMPTY_LINE))
-            if not self.isReplay:
-                self.dialogs.showUpdateFinished(self.i18n['titleOK'], self.i18n['messageOK'].format(version) + git_message)
-        else:
-            if not self.isReplay:
-                Waiting.show(WAITING_UPDATE)
-            url = URLS.UPDATE + ZIP.format(version)
-            logInfo(LOG_MESSAGES.STARTED, version, url)
-            self.downloader = WebDownloader(1)
-            self.downloader.download(url, self.onDownloaded)
+            return self.updateFiles(path, version)
 
-    def closeDownloader(self):
-        if self.downloader is not None:
-            self.downloader.close()
-            self.downloader = None
+        if not self.isReplay:
+            Waiting.show(WAITING_UPDATE)
+        downloader = WebDownloader(1)
+
+        def onUpdateDownloaded(_url, data):
+            if data is not None:
+                with open(path, "wb") as zipArchive:
+                    zipArchive.write(data)
+                logInfo(LOG_MESSAGES.FINISHED, path)
+                self.extractZipArchive(path)
+                git_message = re.sub(r'^\s+|\r|\t|\s+$', GLOBAL.EMPTY_LINE, self.updateData.get('body', GLOBAL.EMPTY_LINE))
+                if not self.isReplay:
+                    self.dialogs.showUpdateFinished(self.i18n['titleOK'], self.i18n['messageOK'].format(version) + git_message)
+            else:
+                self.downloadError(_url)
+            if not self.isReplay and Waiting.isOpened(WAITING_UPDATE):
+                Waiting.hide(WAITING_UPDATE)
+            downloader.close()
+
+        url = URLS.UPDATE + ZIP.format(version)
+        logInfo(LOG_MESSAGES.STARTED, version, url)
+        downloader.download(url, onUpdateDownloaded)
+
+    def updateFiles(self, path, version):
+        self.extractZipArchive(path)
+        logInfo(LOG_MESSAGES.ALREADY_DOWNLOADED, path)
+        git_message = re.sub(r'^\s+|\r|\t|\s+$', GLOBAL.EMPTY_LINE, self.updateData.get('body', GLOBAL.EMPTY_LINE))
+        if not self.isReplay:
+            self.dialogs.showUpdateFinished(self.i18n['titleOK'], self.i18n['messageOK'].format(version) + git_message)
 
     def extractZipArchive(self, path):
         old_files = os.listdir(self.modPath)
@@ -101,7 +114,7 @@ class DownloadThread(object):
                     logInfo(LOG_MESSAGES.NEW_FILE, newFile)
 
     def fini(self):
-        if not self.hasCleanupFile:
+        if not self.cleanup_launcher_exist:
             return
         version = self.updateData.get('tag_name', self.version)
         to_delete = [str(os.path.join(self.modPath, old_file))
@@ -113,23 +126,6 @@ class DownloadThread(object):
             CREATE_NO_WINDOW = 0x08000000
             args = [self.cleanup_exe] + to_delete
             subprocess.Popen(args, creationflags=DETACHED_PROCESS | CREATE_NO_WINDOW, close_fds=True)
-
-    def onDownloaded(self, _url, data):
-        if data is not None:
-            version = self.updateData.get('tag_name', self.version)
-            path = os.path.join(getUpdatePath(), version + ".zip")
-            with open(path, "wb") as zipArchive:
-                zipArchive.write(data)
-            logInfo(LOG_MESSAGES.FINISHED, path)
-            self.extractZipArchive(path)
-            git_message = re.sub(r'^\s+|\r|\t|\s+$', GLOBAL.EMPTY_LINE, self.updateData.get('body', GLOBAL.EMPTY_LINE))
-            if not self.isReplay:
-                self.dialogs.showUpdateFinished(self.i18n['titleOK'], self.i18n['messageOK'].format(version) + git_message)
-        else:
-            self.downloadError(_url)
-        if not self.isReplay and Waiting.isOpened(WAITING_UPDATE):
-            Waiting.hide(WAITING_UPDATE)
-        self.closeDownloader()
 
     def downloadError(self, url):
         message = LOG_MESSAGES.FAILED.format(url)
@@ -156,7 +152,7 @@ class Updater(DownloadThread):
         if not self.isReplay:
             ServicesLocator.appLoader.onGUISpaceEntered -= self.onGUISpaceEntered
 
-    def responseUpdate(self, response):
+    def responseUpdateCheck(self, response):
         if response.responseCode == HTTP_OK_STATUS:
             response_data = loads(response.body)
             self.updateData.update(response_data)
@@ -166,7 +162,7 @@ class Updater(DownloadThread):
                 if not self.isReplay:
                     self.showUpdateDialog(new_version)
                 else:
-                    self.startDownload(new_version)
+                    self.startDownloadingUpdate(new_version)
             else:
                 logInfo(LOG_MESSAGES.UPDATE_CHECKED)
         elif response.responseCode == 304:
@@ -178,7 +174,7 @@ class Updater(DownloadThread):
     def check(self):
         logInfo(LOG_MESSAGES.CHECK)
         response = yield async_url_request(URLS.UPDATE_GITHUB_API_URL)
-        self.responseUpdate(response)
+        self.responseUpdateCheck(response)
 
     @staticmethod
     def tupleVersion(version):
@@ -200,4 +196,4 @@ class Updater(DownloadThread):
         handle_url = URLS.UPDATE + EXE_FILE.format(ver)
         result = yield wg_await(self.dialogs.showNewVersionAvailable(title, message, handle_url))
         if result:
-            self.startDownload(ver)
+            self.startDownloadingUpdate(ver)
