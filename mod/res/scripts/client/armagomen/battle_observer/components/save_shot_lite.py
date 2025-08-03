830904b70ac4377e444d8c6bb58938cfcedec386
Armagomen <armagomen@gmail.com>
2025-08-03 16:53:50 +0300

    fixed starting save shoot enable on client start

--- mod/res/scripts/client/armagomen/battle_observer/components/save_shot_lite.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/save_shot_lite.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import TriggersManager
-from armagomen._constants import MAIN, VEHICLE
+from armagomen._constants import MAIN
 from armagomen.battle_observer.i18n.save_shoot import LOCKED_MESSAGE
 from armagomen.battle_observer.settings import user_settings
 from armagomen.utils.common import cancelOverride, overrideMethod
@@ -28,12 +28,12 @@ class SaveShootLite(TriggersManager.ITriggerListener):
         self.enabled = user_settings.main[MAIN.SAVE_SHOT]
         self.unlock = False
         self.vehicleErrorComponent = None
-        overrideMethod(PlayerAvatar, "shoot")(self.shoot)
 
     def shoot(self, base, avatar, isRepeat=False):
-        if isRepeat or self.unlock or self.checkTarget(avatar):
+        target = avatar.target
+        if target is None or isRepeat or self.unlock or self.checkTarget(target):
             return base(avatar, isRepeat=isRepeat)
-        vehicle_info = self.sessionProvider.getArenaDP().getVehicleInfo(avatar.target.id)
+        vehicle_info = self.sessionProvider.getArenaDP().getVehicleInfo(target.id)
         message = LOCKED_MESSAGE.format(vehicle_info.vehicleType.shortName)
         g_instance.gui.addClientMessage(message)
         if self.vehicleErrorComponent is not None:
@@ -70,10 +70,8 @@ class SaveShootLite(TriggersManager.ITriggerListener):
             self.unlock = False
 
     @staticmethod
-    def checkTarget(avatar):
-        target = avatar.target
-        return target is None or isinstance(target, (Vehicle, DestructibleEntity)) and target.isAlive() and target.publicInfo[
-            VEHICLE.TEAM] != avatar.team
+    def checkTarget(target):
+        return isinstance(target, (Vehicle, DestructibleEntity)) and target.isAlive() and not target.isPlayerTeam()
 
     def keyEvent(self, isKeyDown):
         self.unlock = isKeyDown
