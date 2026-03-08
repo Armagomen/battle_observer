85c1c47ab48eb0b804f1820940b713e8d21a7965
Kurzdor <github@kurzdor.me>
2026-03-08 14:36:52 +0200

    fix(excluded_maps): upstream to WoT 2.2 changes

--- mod/res/scripts/client/armagomen/battle_observer/components/excluded_maps.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/excluded_maps.py
@@ -2,9 +2,13 @@ from armagomen._constants import EXCLUDED_MAPS, MAIN
 from armagomen.battle_observer.i18n.exluded_maps import EXCLUDED_MAPS_BY_LANG
 from armagomen.utils.dialogs import ExcludedMapsDialog
 from armagomen.utils.events import g_events
-from constants import PREMIUM_TYPE, PremiumConfigs, RENEWABLE_SUBSCRIPTION_CONFIG
+from PlayerEvents import g_playerEvents
+from constants import PREMIUM_TYPE, PremiumConfigs
+from gui.game_control.wot_plus.utils import getExcludedMapsPromoData
 from gui.impl.pub.dialog_window import DialogButtons
 from gui.shared.event_dispatcher import showMapsBlacklistView
+from renewable_subscription_common.schema import renewableSubscriptionsConfigSchema
+from renewable_subscription_common.settings_constants import RS_TIER
 from helpers import dependency
 from skeletons.gui.app_loader import GuiGlobalSpaceID, IAppLoader
 from skeletons.gui.game_control import IGameSessionController, IWotPlusController
@@ -14,8 +18,7 @@ from wg_async import wg_async, wg_await
 
 SERVER_SETTINGS_DIFF_KEYS = (
     PremiumConfigs.IS_PREFERRED_MAPS_ENABLED,
-    PremiumConfigs.PREFERRED_MAPS,
-    RENEWABLE_SUBSCRIPTION_CONFIG
+    PremiumConfigs.PREFERRED_MAPS
 )
 
 
@@ -32,6 +35,7 @@ class ExcludedMapsProcessor(object):
         self.__isDialogVisible = False
         self.appLoader.onGUISpaceEntered += self.onGUISpaceEntered
         self.appLoader.onGUISpaceLeft += self.onGUISpaceLeft
+        g_playerEvents.onConfigModelUpdated += self._onConfigModelUpdated
         g_events.onModSettingsChanged += self._onModSettingsChanged
 
     def fini(self):
@@ -61,6 +65,10 @@ class ExcludedMapsProcessor(object):
         self.gameSession.onPremiumNotify -= self.__onPremiumNotify
         self.wotPlus.onDataChanged -= self.__onWotPlusChanged
 
+    def _onConfigModelUpdated(self, gpKey):
+        if renewableSubscriptionsConfigSchema.gpKey == gpKey:
+            self.__update()
+
     def _onModSettingsChanged(self, name, data):
         if name == MAIN.NAME and self.__enabled != data[MAIN.EXCLUDED_MAP_SLOTS_NOTIFICATION]:
             self.__enabled = data[MAIN.EXCLUDED_MAP_SLOTS_NOTIFICATION]
@@ -76,7 +84,7 @@ class ExcludedMapsProcessor(object):
             self.__update()
 
     def __onWotPlusChanged(self, data):
-        if 'isEnabled' in data:
+        if RS_TIER in data:
             self.__update()
 
     @staticmethod
@@ -97,10 +105,11 @@ class ExcludedMapsProcessor(object):
             return
         mapsConfig = self._serverSettings.getPreferredMapsConfig()
         usedSlots = sum(int(mapId > 0) for mapId, _ in self.itemsCache.items.stats.getMapsBlackList())
+        isWotPlusAcc, wotPlusSlots = getExcludedMapsPromoData()
         totalSlots = sum([
             mapsConfig['defaultSlots'],
             mapsConfig['premiumSlots'] if self.__isPremium else 0,
-            mapsConfig['wotPlusSlots'] if self.wotPlus.isEnabled() and self._serverSettings.isWotPlusExcludedMapEnabled() else 0
+            wotPlusSlots if isWotPlusAcc else 0
         ])
         if usedSlots < totalSlots:
             self.__showDialog(self.__getLocalizedMessage(totalSlots - usedSlots))
