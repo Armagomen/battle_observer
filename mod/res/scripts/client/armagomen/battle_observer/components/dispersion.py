c59c484b84b9b882b936e89441869091d975f039
armagomen <armagomen@gmail.com>
2026-06-23 13:08:48 +0300

    dispersion

--- mod/res/scripts/client/armagomen/battle_observer/components/dispersion.py
+++ mod/res/scripts/client/armagomen/battle_observer/components/dispersion.py
@@ -15,8 +15,6 @@ from helpers import dependency
 from skeletons.account_helpers.settings_core import ISettingsCore
 from VehicleGunRotator import VehicleGunRotator
 
-DEV_FACTORIES_COLLECTION = (gm_factory._DevControlMarkersFactory, gm_factory._OptionalMarkersFactory, gm_factory._EquipmentMarkersFactory)
-
 debug_const = "Debug"
 for key, value in gm_factory._GUN_MARKER_LINKAGES.items():
     if debug_const in key:
@@ -24,16 +22,15 @@ for key, value in gm_factory._GUN_MARKER_LINKAGES.items():
 
 REPLACE_TYPES = {gun_marker_ctrl._MARKER_TYPE.CLIENT, gun_marker_ctrl._MARKER_TYPE.DUAL_ACC}
 
-settingsLoder = dependency.instance(IBOSettingsLoader)
 
-
-def get_dispersion_scale_setting(marker_type):
+def get_dispersion_scale_setting(gunMakerType):
+    settingsLoder = dependency.instance(IBOSettingsLoader)
     replace_setting = settingsLoder.getSetting(DISPERSION.NAME, DISPERSION.REPLACE)
     server_setting = settingsLoder.getSetting(DISPERSION.NAME, DISPERSION.SERVER)
     result = False
-    if marker_type == gun_marker_ctrl._MARKER_TYPE.SERVER:
+    if gunMakerType == gun_marker_ctrl._MARKER_TYPE.SERVER:
         result = replace_setting or server_setting
-    elif marker_type in REPLACE_TYPES:
+    elif gunMakerType in REPLACE_TYPES:
         result = replace_setting
     return float(settingsLoder.getSetting(DISPERSION.NAME, DISPERSION.SCALE)) if result else 1.0
 
@@ -50,6 +47,9 @@ class _DefaultGunMarkerController(gun_marker_ctrl._DefaultGunMarkerController):
 
 
 class _DualAccMarkerController(_DefaultGunMarkerController):
+
+    def _getMarkerSize(self, gunMarkerInfo):
+        return gunMarkerInfo.dualAccSize
 
     def _replayReader(self, replayCtrl):
         return replayCtrl.getDualAccMarkerSize
@@ -83,15 +83,27 @@ class DispersionCircle(object):
         self.enabled = False
         g_events.onModSettingsChanged += self.onModSettingsChanged
 
+        self.__server_overrides = (
+            (gm_factory, "createComponents", self.createOverrideComponents),
+            (gm_factory, "overrideComponents", self.createOverrideComponents),
+            (gun_marker_ctrl, "useDefaultGunMarkers", self.useDefaultGunMarkers),
+            (gun_marker_ctrl, "useClientGunMarker", self.useGunMarker),
+            (gun_marker_ctrl, "useServerGunMarker", self.useGunMarker),
+            (VehicleGunRotator, "applySettings", self.onPass),
+            (VehicleGunRotator, "setShotPosition", self.setShotPosition),
+            (CrosshairDataProxy, "__onServerGunMarkerStateChanged", self.onPass),
+            (CrosshairPanelContainer, "setGunMarkerColor", self.setGunMarkerColor)
+        )
+
     def fini(self):
         g_events.onModSettingsChanged -= self.onModSettingsChanged
 
-    def createOverrideComponents(self, base, *args):
-        self.disableWGServerMarker()
+    @staticmethod
+    def createOverrideComponents(base, *args):
         BigWorld.player().cell.setServerMarker(True)
-        if len(args) == 2:
-            return gm_factory._GunMarkersFactories(*DEV_FACTORIES_COLLECTION).create(*args)
-        return gm_factory._GunMarkersFactories(*DEV_FACTORIES_COLLECTION).override(*args)
+        factory = gm_factory._GunMarkersFactories(gm_factory._DevControlMarkersFactory, gm_factory._OptionalMarkersFactory,
+                                                  gm_factory._EquipmentMarkersFactory)
+        return factory.create(*args) if len(args) == 2 else factory.override(*args)
 
     @staticmethod
     def useDefaultGunMarkers(*args, **kwargs):
@@ -137,47 +149,28 @@ class DispersionCircle(object):
             self.server = data.get(DISPERSION.SERVER, self.server)
             self.toggleLimiter(self.limiter)
             self.toggleServerCrossOverrides(self.server)
-            toggleOverride(gun_marker_ctrl, "createGunMarker", self.createGunMarker_WG, self.replace or self.server)
+            toggleOverride(gun_marker_ctrl, "createGunMarker", self.__createGunMarker, self.replace or self.server)
         else:
-            toggleOverride(gun_marker_ctrl, "createGunMarker", self.createGunMarker_WG, False)
+            toggleOverride(gun_marker_ctrl, "createGunMarker", self.__createGunMarker, False)
             self.toggleServerCrossOverrides(False)
             self.toggleLimiter(False)
 
     def toggleServerCrossOverrides(self, enable):
-        server_overrides = (
-            (gm_factory, "createComponents", self.createOverrideComponents),
-            (gm_factory, "overrideComponents", self.createOverrideComponents),
-            (gun_marker_ctrl, "useDefaultGunMarkers", self.useDefaultGunMarkers),
-            (gun_marker_ctrl, "useClientGunMarker", self.useGunMarker),
-            (gun_marker_ctrl, "useServerGunMarker", self.useGunMarker),
-            (VehicleGunRotator, "applySettings", self.onPass),
-            (VehicleGunRotator, "setShotPosition", self.setShotPosition),
-            (CrosshairDataProxy, "__onServerGunMarkerStateChanged", self.onPass),
-            (CrosshairPanelContainer, "setGunMarkerColor", self.setGunMarkerColor)
-        )
-        for obj, method_name, func in server_overrides:
+        for obj, method_name, func in self.__server_overrides:
             toggleOverride(obj, method_name, func, enable)
 
-    def createGunMarker_WG(self, baseCreateGunMarker, isStrategic):
+    def __createGunMarker(self, base, isStrategic):
+        if self.server:
+            self.disableWGServerMarker()
+        factory = gun_marker_ctrl._GunMarkersDPFactory()
         if isStrategic:
-            return self.createStrategicGunMarker()
+            client = SPGController(gun_marker_ctrl._MARKER_TYPE.CLIENT, factory.getClientSPGProvider())
+            server = SPGController(gun_marker_ctrl._MARKER_TYPE.SERVER, factory.getServerSPGProvider())
+            dual = gun_marker_ctrl._EmptyGunMarkerController(gun_marker_ctrl._MARKER_TYPE.UNDEFINED, None)
         else:
-            return self.createDefaultGunMarker()
-
-    @staticmethod
-    def createDefaultGunMarker(*args):
-        factory = gun_marker_ctrl._GunMarkersDPFactory()
-        client = _DefaultGunMarkerController(gun_marker_ctrl._MARKER_TYPE.CLIENT, factory.getClientProvider())
-        server = _DefaultGunMarkerController(gun_marker_ctrl._MARKER_TYPE.SERVER, factory.getServerProvider())
-        dual = _DualAccMarkerController(gun_marker_ctrl._MARKER_TYPE.DUAL_ACC, factory.getDualAccuracyProvider())
-        return gun_marker_ctrl._GunMarkersDecorator(client, server, dual)
-
-    @staticmethod
-    def createStrategicGunMarker(*args):
-        factory = gun_marker_ctrl._GunMarkersDPFactory()
-        client = SPGController(gun_marker_ctrl._MARKER_TYPE.CLIENT, factory.getClientSPGProvider())
-        server = SPGController(gun_marker_ctrl._MARKER_TYPE.SERVER, factory.getServerSPGProvider())
-        dual = gun_marker_ctrl._EmptyGunMarkerController(gun_marker_ctrl._MARKER_TYPE.UNDEFINED, None)
+            client = _DefaultGunMarkerController(gun_marker_ctrl._MARKER_TYPE.CLIENT, factory.getClientProvider())
+            server = _DefaultGunMarkerController(gun_marker_ctrl._MARKER_TYPE.SERVER, factory.getServerProvider())
+            dual = _DualAccMarkerController(gun_marker_ctrl._MARKER_TYPE.DUAL_ACC, factory.getDualAccuracyProvider())
         return gun_marker_ctrl._GunMarkersDecorator(client, server, dual)
 
 
