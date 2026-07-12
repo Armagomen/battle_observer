eb7983eeb329c57efd54047175b8829f55786af3
armagomen <armagomen@gmail.com>
2026-07-12 21:42:50 +0300

    default armor position fix

--- mod/res/scripts/client/armagomen/battle_observer/settings/settings_data.py
+++ mod/res/scripts/client/armagomen/battle_observer/settings/settings_data.py
@@ -145,7 +145,7 @@ class SettingsData(object):
 
         self.armor_calculator = {
             GLOBAL.ENABLED: False,
-            ARMOR_CALC.POSITION: {GLOBAL.X: 0, GLOBAL.Y: 60},
+            ARMOR_CALC.POSITION: {GLOBAL.X: 0, GLOBAL.Y: 100},
             ARMOR_CALC.ON_ALLY: False,
             ARMOR_CALC.SHOW_PIERCING_RESERVE: False,
             ARMOR_CALC.SHOW_COUNTED_ARMOR: True,
