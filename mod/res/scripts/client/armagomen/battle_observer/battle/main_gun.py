c3d4b5629f404e09b8fc126403daec824d213933
armagomen <armagomen@gmail.com>
2026-09-17 00:02:50 +0300

    enemy damage in main gun fix

--- mod/res/scripts/client/armagomen/battle_observer/battle/main_gun.py
+++ mod/res/scripts/client/armagomen/battle_observer/battle/main_gun.py
@@ -56,7 +56,7 @@ class MainGun(MainGunMeta, IBattleFieldListener):
         self.as_gunDataS(self.playerDamage, self.gunScore, self._warning)
 
     def onPlayerDamageChanged(self, attackerID, damage):
-        if damage > self.gunScore and attackerID != self.playerVehicleID:
+        if not self.getVehicleInfo(attackerID).isEnemy() and damage > self.gunScore and attackerID != self.playerVehicleID:
             self.gunScore = damage
             self.updateMainGun()
 
